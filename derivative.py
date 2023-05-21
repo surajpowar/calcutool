903d49e76ef3f91ca364794945137d0b048ed68f
Suraj Powar <47470595+surajpowar@users.noreply.github.com>
2023-05-21 15:44:54 -0400

    Update derivative.py

--- derivative.py
+++ derivative.py
@@ -1,38 +1,13 @@
-import sympy as sym
-from sympy import *
-import matplotlib.pyplot as plt
 import numpy as np
-import math
-
-x = symbols("x")
-
-fx = input("Please enter the function you wish to evaluate: ")
-
-fprimex = diff(fx, x)
+import sympy as smp
+from sympy import *
+import scipy
+from scipy.integrate import quad
+from math import *
 
 
-def newtons(x):
-    h = fx / fprimex
-    while abs(h) >= 0.000001:
-        h = fx/fprimex
-        x = x - h
-        print(x)
-     
-    print("The value of the root is : ", "%.4f"% x)
-    x_plot = np.linspace(-2, 2, 1000)
-    y_plot = fx_plot
- 
 
-    fig = plt.figure()
-    plt.plot(x_plot, y_plot, c="blue")
-    plt.plot(fx, fprimex, c="red", marker="o", fillstyle="none")
-    plt.xlim([-5, 5])
-    plt.ylim([-15, 15])
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Newton's Method")
-    plt.grid()
-    plt.show()
+
 
 
  
