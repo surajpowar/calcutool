215d100e21770d2b47dbff6336fb7a4b28db1981
Suraj Powar <47470595+surajpowar@users.noreply.github.com>
2023-05-22 14:40:44 -0400

    Update calcutool.py

--- calcutool.py
+++ calcutool.py
@@ -7,18 +7,7 @@ from sympy import *
 import scipy
 from scipy.integrate import quad
 from math import *
+import subprocess
 
-question = input("Do you want to take a derivative or integrate a function (press d for derivative or i for integration): ")
 
-if question == str("d"):
-    f = input("Enter the function you wish to take a derivative of: ")
-    x = symbols('x')
-    fprime = diff(f, x)
-    print("The derivative of the function you entered is: ",fprime)
-else:
-    p = input("Enter the function you wish to integrate: ")
-    b = input("Enter upper bound: ")
-    a = input("Enter lower bound: ")
-    i = quad(p, a, b)
-    print("The integration value is: ", i)
 
