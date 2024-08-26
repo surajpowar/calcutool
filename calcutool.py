42252a93a13cd30520bfd429bc45ab76577a42f8
Suraj Powar <47470595+surajpowar@users.noreply.github.com>
2024-08-26 13:19:05 -0500

    Add files via upload

--- calcutool.py
+++ calcutool.py
@@ -5,9 +5,22 @@ import numpy as np
 import sympy as smp
 from sympy import *
 import scipy
-from scipy.integrate import quad
+from scipy.integrate import quads
 from math import *
-import subprocess
+import subprocess 
+from subprocess import call 
+from subprocess import Popen
+from integration import integration as main1
+from derivative import derivative as main2
 
+def run(runfile):
+  with open(runfile,"r") as rnf:
+    exec(rnf.read())
 
-
+input1 = input("Do you wish to integrate a function or derivate? [Type integrate or derivate]: ").lower()
+if input1 == "integrate":
+    run("integration.py")
+elif input1 == "derivate":
+    run("derivative.py")
+else:
+    print("Please type either integrate or derivate for using the package.")
